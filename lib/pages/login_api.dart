a731ea6b39281cf2a117ab992242c6fc9513dcc0
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2020-04-23 20:31:52 -0300

    Parser de JSON (simples) retornado no Login

--- lib/pages/login_api.dart
+++ lib/pages/login_api.dart
@@ -22,12 +22,16 @@ class LoginApi {
       print('Response status: ${response.statusCode}');
       print('Response body: ${response.body}');
 
-      
+      Map mapResponse = json.decode(response.body);
+      String nome = mapResponse["nome"];
+      String email = mapResponse["email"];
+
+      print("Nome: $nome");
+      print("E-mail: $email");
+
     } catch (e) {
       print(e);
     }
-
-    //Map mapResponse = json.decode(response.body);
 
     return true;
   }
