76349183e4a94216d800febebd1ee88b658da636
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2020-04-23 20:08:02 -0300

    Login WebService 1

--- lib/pages/login_api.dart
+++ lib/pages/login_api.dart
@@ -1,34 +1,19 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 
 class LoginApi {
-  static bool login(String login, String senha){
+  static Future<bool> login(String login, String senha) async{
     //var url = 'https://carros-springboot.heroku.com/api/v2/login';
     var url = "http://livrowebservices.com.br/rest/login";
-    /*Map<String, String> headers ={
-      "Content-Type" : "application/json"
+
+    Map params = {
+      "username": "user",
+      "password": "123"
     };
 
-    Map params ={
-      "username": login,
-      "password": senha
-    };
-
-    String s = json.encode(params);
-
-    try {
-      var response = await http.post(url, body: s, headers: headers);
-      print('Response status: ${response.statusCode}');
-      print('Response body: ${response.body}');
-      Map mapResponse = json.decode(response.body);
-      String nome = mapResponse["nome"];
-      String email = mapResponse["email"];
-      print(nome);
-      print(email);
-    } catch (e) {
-      print(e);
-    }*/
+    var response = await http.post(url, body: params);
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
 
     return true;
   }
